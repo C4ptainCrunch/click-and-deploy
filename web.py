ef63988bc4b40ebd47d13c7207b1331883a93e05
Nikita Marchant <nikita.marchant@gmail.com>
2015-12-03 12:11:01 +0100

    Fix json error in python2

--- web.py
+++ web.py
@@ -5,6 +5,10 @@ from rq import Queue
 from redis import Redis
 import json
 
+import sys
+
+JSONError = json.JSONDecodeError if sys.version_info.major >= 3 else ValueError
+
 flapp = Flask(__name__)
 flapp.debug = settings.DEBUG
 
@@ -36,7 +40,7 @@ def deploy_app(app_id):
             data = json.loads(request.data)
             if data['ref'].split('/')[-1] != "master":
                 return "Not a usefull branch : {}".format(data['ref'])
-        except (json.JSONDecodeError, KeyError) as e:
+        except (json.JSONError, KeyError) as e:
             print(e)
     lib.app_exists(app_id)
     q.enqueue(lib.deploy_app, app_id)
